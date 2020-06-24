66f6b6eb4c20631d5f052b5a6b547efa6f27604f
Ian Andre Aragon Saenz <ianaragon@fb.com>
2020-06-24 11:42:44 -0500

    Initial Commit

---
+++ Flix/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Flix
+//
+//  Created by Ian Andre Aragon Saenz on 24/06/20.
+//  Copyright © 2020 IanAragon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Flix/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Flix
+//
+//  Created by Ian Andre Aragon Saenz on 24/06/20.
+//  Copyright © 2020 IanAragon. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
